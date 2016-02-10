056644235c3b3e06ae3d75558884c9b9a3d53d1e
makpaaal <serikkaliyeva.makpal@gmail.com>
2016-02-10 14:26:50 +0600

    class Student

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -17,23 +17,41 @@ namespace Complex
         public static Complex operator +(Complex first, Complex second)
         {
             Complex third = new Complex(first.a + second.a, first.b + second.b);
-                return third;
+            return third;
         }
 
         public override string ToString()
-         {
+        {
             return a + "/" + b;
         }
-   
+
     }
 
-    class Program {
-            static void Main(string[] argc)
+    class Program
+    {
+        static void Main(string[] argc)
         {
-            Console.WriteLine(new Complex(1,3)+ new Complex(1, 4));
+            Console.WriteLine(new Complex(1, 3) + new Complex(1, 4));
             Console.ReadKey();
         }
     }
+    class Student
+    {
+        public string Name, Surname;
+        public double gpa;
+        public Student(string Name, string Surname, double gpa)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.gpa = gpa;
+        }
+     
+         public override string ToString()
+        {
+            
+            return this.Name + " " + Surname + " " + gpa;
+        }
+    }
 }
-
+    
  
