5777e4e7b7408d998e266970f264b3fbc5162ad9
makpaaal <serikkaliyeva.makpal@gmail.com>
2016-02-10 13:25:54 +0600

    Prime numbers

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            {
+                for (int i = 2; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        Console.WriteLine("This number is not prime");
+                        Console.ReadKey();
+
+                    }
+                    {
+                        Console.WriteLine("This number is prime");
+                        Console.ReadKey();
+                    }
+                }
+            }
+        }
+    }
+}
